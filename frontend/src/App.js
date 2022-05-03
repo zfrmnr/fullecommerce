a7c3d28ac4b02019ee62da0ce0c45f71d7baf9cd
zfrmnr <zfrmnr@gmail.com>
2022-05-03 20:47:14 +0300

    FullECommerce-Create-react-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Ecommerce</a>
+      </header>
+      <main>list of products</main>
+    </div>
+  );
+}
+
+export default App;
