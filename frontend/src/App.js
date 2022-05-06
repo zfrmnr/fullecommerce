0f7b679f775e28cf94e6bca5fc098bafd69f73e0
zfrmnr <zfrmnr@gmail.com>
2022-05-07 02:30:02 +0300

    helmet lib and product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,37 @@
+//import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Ecommerce</a>
-      </header>
-      <main>list of products</main>
-    </div>
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Ecommerce</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">all right deserve</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
